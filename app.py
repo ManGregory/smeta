ff5ef5aedd28a07aef30610ab251afd7ac6472d3
Manaenkov Grigoriy <manaenkov.gregory@gmail.com>
2014-09-14 18:04:53 +0300

    changed login page, added flash block to base template

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template, flash, redirect, session, url_for, request, g
 from forms import LoginForm
+from flask.ext.openid import OpenID
+from config import basedir
 
 # initialization
 app = Flask(__name__)
@@ -8,6 +10,7 @@ app.config.from_object('config')
 app.config.update(
     DEBUG = True
 )
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 # controllers
 @app.route("/")
@@ -24,14 +27,28 @@ def index():
 	return render_template("index.html", projects = projects)
 
 @app.route("/login", methods = ['GET', 'POST'])
+@oid.loginhandler
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
-	 	return redirect(url_for('index'))
+		session['remember_me'] = form.remember_me.data		
+		return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])		
 	return render_template('login.html',
 		title = 'Sign in',
 		form = form,
 		providers = app.config['OPENID_PROVIDERS'])
+
+@oid.after_login
+def after_login(resp):
+	if resp.email is None or resp.email == "":
+		flash('Invalid login. Please try again.', 'danger')
+		return redirect(url_for('login'))
+	remember_me = False
+	if 'remember_me' in session:
+		remember_me = session['remember_me']
+		session.pop('remember_me', None)
+	flash(request.args.get('next'), 'info')
+	return redirect(request.args.get('next') or url_for('index'))
 
 @app.route("/favicon.ico")    
 def favicon():
--- config.py
+++ config.py
@@ -2,7 +2,10 @@ CSRF_ENABLED = True
 SECRET_KEY = '52be9208-e113-476e-9391-9793cc30ea16'
 
 OPENID_PROVIDERS = [
-    { 'name': 'google-plus', 'url': 'https://www.google.com/accounts/o8/id' },
-    { 'name': 'vk', 'url': 'http://vkontakteid.ru/'},
-    { 'name': 'facebook', 'url': 'http://facebook-openid.appspot.com/'}
+    { 'name': 'google-plus', 'display_name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'vk', 'display_name': 'VKontakte', 'url': 'http://vkontakteid.ru/'},
+    { 'name': 'facebook', 'display_name': 'Facebook', 'url': 'http://facebook-openid.appspot.com/'}
 ]
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
